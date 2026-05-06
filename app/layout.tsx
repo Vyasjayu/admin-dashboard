48edca7d0ce1042e9a6f0ae639e4aa6803633709
Vyasjayu <91581573+Vyasjayu@users.noreply.github.com>
2026-05-06 08:58:10 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,11 @@
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+import { Toaster } from "react-hot-toast";
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>
         {children}
+        <Toaster position="top-right" />
       </body>
     </html>
   );
